6c301b93425e58c9456023e039b80cc0e15fcda7
andlogreg <120428961+andlogreg@users.noreply.github.com>
2025-12-19 23:45:47 +0000

    Quartz sync: Dec 19, 2025, 11:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "andlogreg.github.io/digital-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "published",
     theme: {
