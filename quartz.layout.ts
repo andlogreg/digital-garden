13c09594e502ff02c34d0a8d03929f6fd16f48a8
andlogreg <120428961+andlogreg@users.noreply.github.com>
2025-12-21 16:54:24 +0000

    Quartz sync: Dec 21, 2025, 4:54 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,6 +40,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       "🔗 All My Links": "https://links.andlogreg.work",
+      "RSS": "https://andlogreg.work/index.xml",
     },
   }),
 }
