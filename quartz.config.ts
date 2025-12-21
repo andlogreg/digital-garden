252e9b397db70c87a8af08a914ca526d81f6a5f2
andlogreg <120428961+andlogreg@users.noreply.github.com>
2025-12-21 16:56:55 +0000

    Quartz sync: Dec 21, 2025, 4:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -144,6 +144,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
+        rssLimit: 50,
       }),
       Plugin.Assets(),
       Plugin.Static(),
