80f66408aab19a788fab445413903cb4801b7228
andlogreg <120428961+andlogreg@users.noreply.github.com>
2025-12-21 22:04:37 +0000

    Quartz sync: Dec 21, 2025, 10:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Andlogreg's Memory Leaks",
-    pageTitleSuffix: " Where thoughts escape my mental RAM",
+    pageTitleSuffix: " -- Where thoughts escape my mental RAM",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
